1bd3a196c7915c157180bf301e42cf7871f8393c
vlad <kingbingping@gmail.com>
2021-03-06 11:09:18 +0500

    убрал лишний for

--- lesson_4/dz_2/dz_2.py
+++ lesson_4/dz_2/dz_2.py
@@ -7,13 +7,12 @@ def currency_rates(currency):
     resp = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').content
     use_root = ElementTree.fromstring(resp)
     for element in use_root:
-        for value in element:
-            if currency.upper() == value.text:
-                name = element.find('Name').text
-                coin = element.find('Value').text
-                coin = Decimal(coin.replace(',', '.'))
-                print(f'Курс {name} = {coin}  рубля')
-                break
+        if currency.upper() == element.find('CharCode').text:
+            name = element.find('Name').text
+            coin = element.find('Value').text
+            coin = Decimal(coin.replace(',', '.'))
+            print(f'Курс {name} = {coin} рубля')
+            break
 
 
-currency_rates(input('Введите код валюты, Например USD, EUR: '))
+currency_rates('EUR')
