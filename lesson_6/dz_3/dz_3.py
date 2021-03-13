52ac039d51a78a30f86dd94a365a73af2ba6cd78
vlad <kingbingping@gmail.com>
2021-03-13 14:03:34 +0500

    теперь выход 1

--- lesson_6/dz_3/dz_3.py
+++ lesson_6/dz_3/dz_3.py
@@ -1,8 +1,15 @@
 import json
+import itertools
 
 with open('users.csv', encoding='utf-8') as users:
     with open('hobby.csv', encoding='utf-8') as hobby:
-        dict_hob_use = {user.strip().replace(',', ' '): str(next(hobby, None)).strip() for user in users}
+        hob_use = itertools.zip_longest(users, hobby)
+        dict_hob_use = {}
+        for i in hob_use:
+            dict_hob_use.setdefault(str(i[0]).strip().replace(',', ' '), str(i[1]).strip())
 
 with open('result.json', 'w', encoding='utf-8') as result:
-    json.dump(dict_hob_use, result, indent=5, ensure_ascii=False)
+    if 'None' in dict_hob_use:
+        exit(1)
+    else:
+        json.dump(dict_hob_use, result, indent=5, ensure_ascii=False)
