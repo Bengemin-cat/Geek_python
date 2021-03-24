b6a6e89af2649dbc81d397b6c14091e9b7a46d0a
vlad <kingbingping@gmail.com>
2021-03-24 15:03:32 +0500

    @wraps add

--- Lesson_8/dz_3/dz_3.py
+++ Lesson_8/dz_3/dz_3.py
@@ -1,15 +1,21 @@
-def type_loger(func):
-    def loger(*args, **kwargs):
+from functools import wraps
+
+
+def type_logger(func):
+    @wraps(func)
+    def logger(*args, **kwargs):
         t = func(*args, **kwargs)
         print(func.__name__, (*args, type(t)))
         return t
 
-    return loger
+    return logger
 
 
-@type_loger
+@type_logger
 def calc_cube(x):
     return x ** 3
 
 
 calc_cube(3)
+print(calc_cube.__name__)
+help(calc_cube)
