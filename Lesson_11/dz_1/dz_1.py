807c49c1b1ebd82409bb822121a00a326e9db68b
vlad <kingbingping@gmail.com>
2021-04-05 09:12:22 +0500

    первое сделал

--- Lesson_11/dz_1/dz_1.py
+++ Lesson_11/dz_1/dz_1.py
@@ -1,12 +1,39 @@
-class Data:
+import re
 
-    def __init__(self, date):
-        self.date = date
+
+class Data:
+    __re_valid_date = re.compile(r'^\d{1,2}-\d{1,2}-\d{4}')
+
+    def __init__(self, data):
+        self.data = data
 
     @classmethod
-    def take_date(cls, dmy):
-        return cls(dmy.split('-'))
+    def give_namber(cls, date):
+        if Data.__re_valid_date.match(date):
+            return tuple(int(_) for _ in date.split('-'))
+        else:
+            return f'{date} введена не правильно : формат DD-MM-YYYY'
+
+    @staticmethod
+    def valid_date(obj):
+        month_30 = [4, 6, 9, 11]
+        if Data.__re_valid_date.match(obj):
+            d, m, y = obj.split('-')
+            if int(m) > 12:
+                return 'Месяц указан не верно: больше 12'
+            if int(d) > 31:
+                return 'День указан не верно: больше 31'
+            elif int(m) == 2 and int(d) > 28:
+                return f'В феврале нет {d} дня'
+            elif int(m) in month_30 and int(d) > 30:
+                return f'В {m} месяце нет {d} дня'
+            else:
+                return f'{obj} - дата правильная'
+        else:
+            return f'Дата {obj} введена не правильно : формат DD-MM-YYYY'
 
 
-one = Data.take_date("03-04-2021")
-print(one.date)
+print(Data.valid_date('29-р-2021'))
+print(Data.give_namber('02-04-2021'))
+one = Data('02-04-2021')
+print(one.valid_date(one.data))
